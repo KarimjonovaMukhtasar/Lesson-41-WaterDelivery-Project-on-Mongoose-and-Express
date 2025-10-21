741cfe7df0316beae6dc6a75d309df8035710e9e
KarimjonovaMukhtasar <karimjonovamukhtasar2003@gmail.com>
2025-10-21 08:56:50 +0500

    doc:files created,middleware, validate..and other configs are done

---
+++ src/config/middleWare.js
@@ -0,0 +1,4 @@
+export const errorHandler = (req,res,error,next)=>{
+        console.error(error.stack)
+        return res.status(error.status || 500).json({message:error.message|| `SERVER ERROR!`})
+}
